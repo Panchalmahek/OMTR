6ac0e06d695cc06a4b4c11a23d07354e391e9eaa
Panchalmahek <167449735+Panchalmahek@users.noreply.github.com>
2024-12-25 11:11:44 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,369 @@
+﻿<!DOCTYPE html>
+
+
+<head>
+	
+<meta charset="utf-8">
+	
+<meta name="viewport" content="width=devidev-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+	
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>OMTR</title>
+	
+	
+<!-- [ FONT-AWESOME ICON ] 
+        
+=========================================================================================================================-->
+	
+<link rel="stylesheet" type="text/css" href="library/font-awesome-4.3.0/css/font-awesome.min.css">
+
+	
+<!-- [ PLUGIN STYLESHEET ]
+        
+=========================================================================================================================-->
+	
+<link rel="shortcut icon" type="image/x-icon" href="AHM.jpg">
+	
+<link rel="stylesheet" type="text/css" href="css/animate.css">
+	
+<link rel="stylesheet" type="text/css" href="css/owl.carousel.css">
+        
+<link rel="stylesheet" type="text/css" href="css/owl.theme.css">
+	
+<link rel="stylesheet" type="text/css" href="css/magnific-popup.css">
+	
+<!-- [ Boot STYLESHEET ]
+        
+=========================================================================================================================-->
+	
+<link rel="stylesheet" type="text/css" href="library/bootstrap/css/bootstrap-theme.min.css">
+	
+<link rel="stylesheet" type="text/css" href="library/bootstrap/css/bootstrap.css">
+       
+<!-- [ DEFAULT STYLESHEET ] 
+        
+=========================================================================================================================-->
+	
+<link rel="stylesheet" type="text/css" href="css/style.css">
+        
+<link rel="stylesheet" type="text/css" href="css/responsive.css">
+	
+<link rel="stylesheet" type="text/css" href="css/color/rose.css">
+        
+
+</head>
+<body >
+
+<!-- [ LOADERs ]
+
+================================================================================================================================-->
+	
+    <div class="preloader">
+    
+<div class="loader theme_background_color">
+        
+<span></span>
+      
+    
+</div>
+</div>
+<!-- [ /PRELOADER ]
+
+=============================================================================================================================-->
+
+<!-- [WRAPPER ]
+
+=============================================================================================================================-->
+
+<div class="wrapper">
+  
+<!-- [NAV]
+ 
+============================================================================================================================-->    
+   
+<!-- Navigation
+    ==========================================-->
+    
+<nav  class=" nim-menu navbar navbar-default navbar-fixed-top">
+      
+<div class="container">
+        
+<!-- Brand and toggle get grouped for better mobile display -->
+        
+<div class="navbar-header">
+          
+<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+            
+<span class="sr-only">Toggle navigation</span>
+            
+<span class="icon-bar"></span>
+            
+<span class="icon-bar"></span>
+            
+<span class="icon-bar"></span>
+          
+</button>
+            
+<a class="navbar-brand" href="index.html">O<span class="themecolor">M</span>TR</a>
+        
+</div>
+
+        
+<!-- Collect the nav links, forms, and other content for toggling -->
+        
+<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          
+<ul class="nav navbar-nav navbar-right">
+            
+<li><a href="#home" class="page-scroll"><h3>Home</h3></a></li>
+                                   
+<li><a href="#one" class="page-scroll"><h3>Inspiration</h3></a></li>
+                                 
+<li><a href="#" class="page-scroll"><h3>services</h3></a></li>
+            
+<li><a href="#f" class="page-scroll"><h3>Feedback</h3></a></li>
+
+<li><a href="admin/" class="page-scroll"><h3>Login</h3></a></li>
+          
+</ul>
+        
+</div>
+<!-- /.navbar-collapse -->
+      
+</div><!-- /.container-fluid -->
+    
+</nav>
+
+
+   
+<!-- [/NAV]
+ 
+============================================================================================================================--> 
+    
+   
+<!-- [/ahm-HEADING]
+ 
+============================================================================================================================--> 
+   
+<section class="ahm-heading" id="home">
+       
+<div class="overlay">
+           
+<div class="container">
+               
+<div class="row">
+                   
+<div class="ahm-heading-content col-md-12 col-sm-12 text-center">
+        
+<h1 class="ahm-heading-title"><span class="ahm-element themecolor" data-elements=" ONLINE METRO"></span></h1>
+ 
+<h1 class="ahm-heading-title"><span class="ahm-element themecolor" data-elements=" Ticket Reservation"></span></h1>
+       
+<p class="ahm-heading-text"></p>
+        
+<div class="btn-bar">
+          
+<a href="reserved.php" class="btn btn-custom theme_background_color">Reserved Now</a>
+                 
+</div>
+      
+</div>
+               
+</div>
+           
+</div>
+       
+</div>  
+      
+   
+</section>
+ 
+
+ 
+ 
+ 
+<!-- [/ABOUTUS]
+ 
+ 
+============================================================================================================================-->
+ 
+ 
+ 
+ 
+ 
+<!-- [INSPIRATION]
+ 
+============================================================================================================================-->
+ 
+<section class="inspiration" id="four">
+     
+<div class="overlay">
+         
+<div class="container">
+             
+<div class="row">
+                
+ <article class="col-md-12 text-center">
+           
+ <div class="intermediate-container">
+             
+ <div class="subheading">
+                 
+ <h4>OMTR<span class="themecolor"></span></h4>
+           
+   </div>
+             
+ <div class="heading">
+              
+  <h2></h2>
+          
+    </div>
+              
+<div class="">
+              
+  <a class="btn btn-custom-outline" href="#"><span>get started</span></a>
+        
+      </div>
+        
+    </div>
+       
+   </article>
+       
+      </div>
+    
+    </div>
+    
+ </div>
+ 
+</section>
+ 
+ 
+
+ 
+<!-- [/SERVICES]
+ 
+============================================================================================================================-->
+
+ 
+ 
+ 
+<!-- [CONTACT]
+ 
+============================================================================================================================-->
+ 
+
+ 
+ 
+<!-- [/CONTACT]
+ 
+============================================================================================================================-->
+ 
+ 
+ 
+<!-- [FOOTER]
+ 
+============================================================================================================================-->
+ 
+
+<footer class="site-footer section-spacing text-center " id="eight">
+    
+  
+<div class="container">
+    
+<div class="row">
+      
+<div class="col-md-4">
+        
+<p class="footer-links"><a href="#">Terms of Use</a> <a href="#">Privacy Policy</a></p>
+      
+</div>
+      
+<div class="col-md-4"> <small>&copy; 2021 Nim. All rights reserved.</small></div>
+      
+<div class="col-md-4"> 
+        
+<!--social-->
+        
+        
+<ul class="social">
+          
+<li><a href="https://twitter.com/" target="_blank"><i class="fa fa-twitter "></i></a></li>
+          
+<li><a href="https://www.facebook.com/" target="_blank"><i class="fa fa-facebook"></i></a></li>
+          
+<li><a href="https://www.youtube.com/" target="_blank"><i class="fa fa-youtube-play"></i></a></li>
+        
+</ul>
+        
+        
+<!--social end--> 
+        
+      
+</div>
+    
+</div>
+  
+</div>
+
+</footer>
+
+ 
+ 
+ 
+<!-- [/FOOTER]
+ 
+============================================================================================================================-->
+ 
+ 
+ 
+
+</div>
+ 
+
+<!-- [ /WRAPPER ]
+
+=============================================================================================================================-->
+
+	
+<!-- [ DEFAULT SCRIPT ] -->
+	
+<script src="library/modernizr.custom.97074.js"></script>
+	
+<script src="library/jquery-1.11.3.min.js"></script>
+        
+<script src="library/bootstrap/js/bootstrap.js"></script>
+	
+<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>	
+	
+<!-- [ PLUGIN SCRIPT ] -->
+        
+<script src="library/vegas/vegas.min.js"></script>
+	
+<script src="js/plugins.js"></script>
+        
+<!-- [ TYPING SCRIPT ] -->
+         
+<script src="js/typed.js"></script>
+         
+<!-- [ COUNT SCRIPT ] -->
+         
+<script src="js/fappear.js"></script>
+       
+<script src="js/jquery.countTo.js"></script>
+	
+<!-- [ SLIDER SCRIPT ] -->  
+        
+<script src="js/owl.carousel.js"></script>
+         
+<script src="js/jquery.magnific-popup.min.js" type="text/javascript"></script>
+        
+<script type="text/javascript" src="js/SmoothScroll.js"></script>
+        
+        
+<!-- [ COMMON SCRIPT ] -->
+	<script src="js/common.js"></script>
+  
+</body>
+
+
+</html>
